16b04577792836fc8c580930ea422c7cad6f1262
yakov matan <yakovmatan@gmail.com>
2025-08-21 11:41:30 +0300

    create function to find sentiment

--- app/processor.py
+++ app/processor.py
@@ -1,26 +1,48 @@
 from collections import Counter
 from pandas import DataFrame
-
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from app.fetcher import DataLoader
 
 
 class TextProcessing:
 
     def __init__(self, df: DataFrame):
-
+        nltk.download('vader_lexicon')
         self.df = df.copy()
 
     @staticmethod
-    def rarest_word_in_row(text):
+    def _rarest_word_in_text(text):
         words = text.split()
         counts = Counter(words)
-        min_freq = min(counts.values())
+        min_shows = min(counts.values())
         for w in words:
-            if counts[w] == min_freq:
+            if counts[w] == min_shows:
                 return w
 
-    def word_rarest(self):
-        self.df['rarest_word'] = self.df['Text'].apply(TextProcessing.rarest_word_in_row)
+    @staticmethod
+    def _sentiment_of_text(text):
+        score = SentimentIntensityAnalyzer().polarity_scores(text)
+        if score['compound'] >= 0.5:
+            return 'positive'
+        elif score['compound'] >= -0.49:
+            return "neutral"
+        else:
+            return "negative"
+
+    def rarest_word(self):
+        self.df['rarest_word'] = self.df['Text'].apply(TextProcessing._rarest_word_in_text)
         return self
+
+    def sentiment(self):
+        self.df['sentiment'] = self.df['Text'].apply(TextProcessing._sentiment_of_text)
+        return self
+
+if __name__ == '__main__':
+    d = DataLoader()
+    c = d.get_data()
+    t = TextProcessing(DataFrame(c))
+    print(t.sentiment().df)
 
 
 
